08d3cb725c61ced091be30d70b24ff608fc8adde
Thiago <thiagofacul@gmail.com>
2016-02-15 00:22:28 -0200

    - FAB Menu - Assets

--- app/src/main/java/br/com/trama/cataloga/ui/ListBooksFragment.java
+++ app/src/main/java/br/com/trama/cataloga/ui/ListBooksFragment.java
@@ -1,7 +1,9 @@
 package br.com.trama.cataloga.ui;
 
 import android.support.v4.app.Fragment;
+import android.widget.Toast;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 
 import br.com.trama.cataloga.R;
@@ -11,5 +13,16 @@ import br.com.trama.cataloga.R;
  */
 @EFragment(R.layout.fragment_list_books)
 public class ListBooksFragment extends Fragment {
+
+
+    @Click(R.id.fabTop)
+    void fabTopClick(){
+        Toast.makeText(getActivity(), "fab top", Toast.LENGTH_SHORT).show();
+    }
+
+    @Click(R.id.fabMid)
+    void fabMidClick(){
+        Toast.makeText(getActivity(), "fab mid", Toast.LENGTH_SHORT).show();
+    }
 
 }
