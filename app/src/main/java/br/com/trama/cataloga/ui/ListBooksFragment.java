9f7182905842eb66f3c34267277201a550604a52
trama <thiagofacul@gmail.com>
2016-02-23 23:08:40 -0300

    create card and request image

--- app/src/main/java/br/com/trama/cataloga/ui/ListBooksFragment.java
+++ app/src/main/java/br/com/trama/cataloga/ui/ListBooksFragment.java
@@ -1,12 +1,22 @@
 package br.com.trama.cataloga.ui;
 
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+
+import java.util.Collections;
 
 import br.com.trama.cataloga.R;
+import br.com.trama.cataloga.model.Books;
+import br.com.trama.cataloga.ui.adapters.ListBooksAdapter;
+import br.com.trama.cataloga.util.Mocks;
 
 /**
  * Created by thiago on 11/02/16.
@@ -14,6 +24,16 @@ import br.com.trama.cataloga.R;
 @EFragment(R.layout.fragment_list_books)
 public class ListBooksFragment extends Fragment {
 
+    @ViewById(R.id.fragment_listbooks_recyclerview)
+    RecyclerView recyclerView;
+
+
+    @AfterViews
+    public void init(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(new ListBooksAdapter(Mocks.getBooks(100)));
+    }
 
     @Click(R.id.fabTop)
     void fabTopClick(){
